45964cdf84f152c69da11eb898c0baf70c7cbea6
Andrew Usher <andrewusher00@gmail.com>
2018-04-12 11:56:09 -0500

    Add new site from gatsby-portfolio

---
+++ path----2ed141a961f240082607.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{187:function(t,e){t.exports={data:{site:{siteMetadata:{name:"Andrew Usher"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----2ed141a961f240082607.js.map
